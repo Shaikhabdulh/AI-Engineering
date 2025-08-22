4ca6695f1e49523b5260bd3a507cfa46bc9a338b
ShaikhAbdulHamid MohammadHanif <hamidshaikh05660@gmail.com>
2025-08-22 07:05:28 +0000

    Working on Functions

---
+++ Python_Introduction/01_Functions_and_Variables/Functions/P1.py
@@ -0,0 +1,6 @@
+print("""
+print ("Hello Wolrd")
+"print" is build in function
+"Hello World" is argument 
+if you run this file the "output" will be the *SIDE EFFECT* or outcome of this function.
+""")
