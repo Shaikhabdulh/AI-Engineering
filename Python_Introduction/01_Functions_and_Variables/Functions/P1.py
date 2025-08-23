c5cefcf06254f5422712c087e142170deb50f592
ShaikhAbdulHamid MohammadHanif <hamidshaikh05660@gmail.com>
2025-08-23 09:15:08 +0000

    P1.py Practice

--- Python_Introduction/01_Functions_and_Variables/Functions/P1.py
+++ Python_Introduction/01_Functions_and_Variables/Functions/P1.py
@@ -4,3 +4,9 @@ print ("Hello Wolrd")
 "Hello World" is argument 
 if you run this file the "output" will be the *SIDE EFFECT* or outcome of this function.
 """)
+name=input("Please Insert your name : - ")
+# print("hello, "+name) # + operator Allow you to Connect two strings means concatination.
+print ("hello, ", end=" ") # end peremeter allow you change default behaviour.
+print (name)# What is Parameter are just place holder and argument are value passed by the user or developer.and
+#There are two type of argument 1 named 2 positional arguments
+#named argument are not bound to the sequence and positional arguments are bound with the sequence.
